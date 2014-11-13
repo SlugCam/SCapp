ddf5893f88868fc1242bec3f522a808f5801a44e
Leland Miller <lelandmiller@gmail.com>
2014-11-13 12:36:58 -0800

    Basic alarm area selection working

--- app/scripts/directives/objectlist.js
+++ app/scripts/directives/objectlist.js
@@ -29,15 +29,29 @@ angular.module('myApp')
             scope: {
                 src: '&mySrc'
             },
-            template: '<div><video poster="/images/video_poster.png" width="100%" /></div>',
+            template: '<div class="sc-video-player"><div class="scvp-video-content"><div class="scvp-video-wrapper"><video poster="/images/video_poster.png" width="100%" /></div><div class="scvp-alarm-layer"></div></div><div class="scvp-toolbar"><button class="scvp-set-button">Choose Alarm</button></div></div>',
             link: function(scope, element, attrs) {
+                console.log('Building...');
+
+                var state = 'video'; // 's1 - waiting for first click, s2 waiting for second' 
+
+                element.find('.scvp-set-button').click(function() {
+                    element.find('.scvp-alarm-layer').css('visibility', 'hidden');
+                    element.find('video').removeAttr('controls');
+                    state = 's1';
+                    console.log('set clicked');
+                });
+
+
                 scope.$watch('src()', function(newSrc) {
                     var vid = $('<video controls autoplay/>').attr({
                         'width': '100%',
                         'poster': '/images/video_poster.png'
                     });
-                    element.empty();
-                    vid.appendTo(element);
+
+                    var vidWrapper = element.find('.scvp-video-wrapper');
+                    vidWrapper.empty();
+                    vid.appendTo(vidWrapper);
 
                     if (newSrc !== '') {
                         var src = $('<source />').attr({
@@ -46,6 +60,28 @@ angular.module('myApp')
                         });
                         vid.append(src);
                     }
+
+                    // Setup alarm setting
+                    vid.click(function(e) {
+                        if (state === 's1') {
+                            element.find('.scvp-alarm-layer').css({
+                                'left': e.offsetX,
+                                'top': e.offsetY
+                            });
+                            state = 's2';
+                        } else if (state === 's2') {
+                            element.find('.scvp-alarm-layer').css({
+                                'right': vid.outerWidth() - e.offsetX,
+                                'bottom': vid.outerHeight() - e.offsetY
+                            });
+                            state = 'video';
+                            element.find('.scvp-alarm-layer').css('visibility', 'visible');
+                            element.find('video').attr('controls', '');
+                        }
+
+                        console.dir(e);
+                    });
+                    state = 'video';
                 });
             }
         };
